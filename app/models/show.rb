96ada99da18dda50126f4fe49fbda1727c907430
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-23 15:40:51 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map { |a| a.full_name }
+    end
+
+    def build_network(call_letters)
+        self.network = Network.find_by(call_letters) ? Network.find_by(call_letters) : Network.create(call_letters)
+    end
+
 end
